75c387ac479070ad4676e5a28a8d192faf4e07f6
karnan7 <adithkayalad@gmail.com>
2023-01-18 12:49:43 +0530

    first commit

---
+++ src/components/Icon.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {FaTimes, FaPen, FaRegCircle} from "react-icons/fa";
+
+const Icon = ({name}) => (
+    <div>
+        <h1>
+            <FaTimes className="icon"/>
+        </h1>
+        <h1>
+            <FaRegCircle className="icon"/>
+        </h1>
+        <h1>
+            <FaPen className="icon"/>
+        </h1>
+    </div>
+)
+
+export default Icon;
